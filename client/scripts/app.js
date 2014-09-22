79c442b8783ec6ad2620c6a33e0eb7150a9dcd51
ceg1236 <carlgoldberg1236@gmail.com>
2014-09-22 15:08:28 -0700

    Bower, basic app - fetch, send

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,40 @@
 // YOUR CODE HERE:
 
+var app = {
+  init: function() {},
+  send: function() {
+    $.ajax({
+      url: "https://api.parse.com/1/classes/chatterbox",
+      type: 'POST',
+      data:JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('Message sent');
+      },
+      error: function(data) {
+        console.log('Failed to send');
+      }
+    });
+  },
+  fetch: function() {
+    $.ajax({
+      // url: "https://api.parse.com/1/classes/chatterbox",
+      // ???
+      type: 'GET',
+      // data:JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('Message received');
+      },
+      error: function(data) {
+        console.log('Failed to send');
+      }
+    });
+  },
+  display: function(){}
+} ;
+var message = {
+  username: 'Mel Brooks',
+  text: 'It\'s good to be the king',
+  roomname: 'lobby'
+};
