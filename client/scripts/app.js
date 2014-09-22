afba346c12c402dacb732e77a6676e31092cd570
ceg1236 <carlgoldberg1236@gmail.com>
2014-09-22 16:36:33 -0700

    Init fetch, displays messages, working on send

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,40 +1,77 @@
 // YOUR CODE HERE:
+// $(document).ready(function() {
 
-var app = {
-  init: function() {},
-  send: function() {
-    $.ajax({
-      url: "https://api.parse.com/1/classes/chatterbox",
-      type: 'POST',
-      data:JSON.stringify(message),
-      contentType: 'application/json',
-      success: function(data) {
-        console.log('Message sent');
-      },
-      error: function(data) {
-        console.log('Failed to send');
-      }
-    });
-  },
-  fetch: function() {
-    $.ajax({
-      // url: "https://api.parse.com/1/classes/chatterbox",
-      // ???
-      type: 'GET',
-      // data:JSON.stringify(message),
-      contentType: 'application/json',
-      success: function(data) {
-        console.log('Message received');
-      },
-      error: function(data) {
-        console.log('Failed to send');
-      }
-    });
-  },
-  display: function(){}
-} ;
-var message = {
-  username: 'Mel Brooks',
-  text: 'It\'s good to be the king',
-  roomname: 'lobby'
-};
+
+  var app = {
+    server: "https://api.parse.com/1/classes/chatterbox",
+    init: function() {
+      this.fetch()
+    },
+    send: function() {
+      $.ajax({
+        url: this.server,
+        type: 'POST',
+        data:JSON.stringify(message),
+        contentType: 'application/json',
+        success: function(data) {
+          console.log('Message sent');
+        },
+        error: function(data) {
+          console.log('Failed to send');
+        }
+      });
+    },
+    fetch: function() {
+      $.ajax({
+        url: this.server,
+        type: 'GET',
+        data:JSON.stringify(message),
+        contentType: 'application/json',
+        success: function(data) {
+          $('#chats li').remove();
+
+          $.each(data.results, function(index, msgObj){
+            $('#chats').append('<li>'+ msgObj.username +': '+msgObj.text + ' |from: '+ msgObj.roomname +'</li>');
+          });
+          console.log('Message received');
+        },
+      });
+    },
+    clearMessages: function() {
+      $('#chats').empty();
+    },
+    addMessage: function(msg) {
+      $('#chats').append('<li>'+msg.username+' '+msg.text+
+        ' '+msg.roomname+'</li>');
+    },
+    addRoom: function(rmname) {
+      $('#roomSelect').append("<div class='room'></div>");
+    },
+    display: function(){}
+  } ;
+
+app.init();
+
+
+
+  var message = {
+    username: 'Mel Brooks',
+    text: 'It\'s good to be the king',
+    roomname: 'lobby'
+  };
+// });
+
+// HTML will include a display elmt, an input box,
+//  and submit button.
+//  Display elmt - ul populated w li where each msg
+//                is li
+// Input box - input tag
+// Submit button - when clicked, call
+//                  send function with msg
+//
+// Every so often aka setTimeout...
+  // call clear, fetch, and display to our DOM-display
+  //
+// Event handler - click submit take any input text,
+    //save to var, stringify, put in msg.text prop
+    // pass in var to .send
